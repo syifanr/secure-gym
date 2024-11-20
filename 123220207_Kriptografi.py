8d4b4976c96f1aa9b0b1f5e1932831843d6209fc
Syifa <142104833+syifanr@users.noreply.github.com>
2024-11-20 22:53:10 +0700

    Update 123220207_Kriptografi.py

--- 123220207_Kriptografi.py
+++ 123220207_Kriptografi.py
@@ -312,23 +312,19 @@ def update_completed_bookings_table():
     conn = sqlite3.connect("users.db")
     c = conn.cursor()
 
-    # Periksa apakah kolom 'full_name' ada di tabel 'completed_bookings'
+    # Periksa apakah kolom ada di tabel
     c.execute("PRAGMA table_info(completed_bookings)")
     columns = [column[1] for column in c.fetchall()]
     
-    # Tambahkan kolom 'full_name' jika belum ada
+    # Tambahkan kolom hanya jika belum ada
     if "full_name" not in columns:
         c.execute("ALTER TABLE completed_bookings ADD COLUMN full_name TEXT")
-
-    # Tambahkan kolom 'membership_type' jika belum ada
     if "membership_type" not in columns:
         c.execute("ALTER TABLE completed_bookings ADD COLUMN membership_type TEXT")
 
     conn.commit()
     conn.close()
 
-# Jalankan fungsi update
-update_completed_bookings_table()
 
 def reset_completed_bookings():
     """
